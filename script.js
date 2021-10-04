3b6ccaea22458dc705eb514ef3d35963d8df7ef7
Leonardo Prevost <prevostleonardo@gmail.com>
2021-10-04 16:57:01 -0300

    Added play array compostion

--- script.js
+++ script.js
@@ -2,10 +2,44 @@ document.body.addEventListener('keyup',(e)=>{
     playsound(e.code.toLocaleLowerCase());
 });
 
+document.querySelector('.composer button').addEventListener('click', ()=>{
+    let input = document.querySelector('#input').value;
+
+    if(input !== '') {
+        let inputArray = input.split('');
+        playComposition(inputArray);
+    }
+});
+
+
 function playsound (sound){
     let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key=${sound}]`);
 
     if(audioElement) {
+        audioElement.currentTime = 0;
         audioElement.play();
+    }
+    if(keyElement) {
+        keyElement.classList.add('active');
+
+        setTimeout(()=>{
+            keyElement.classList.remove('active');
+        },300);
+    }
+
+}
+
+function playComposition(inputArray) {
+    let wait = 0;
+
+
+    for(let songItem of inputArray){
+        setTimeout(()=>{
+            playsound(`key${songItem}`);
+        },wait);
+
+        wait += 250;
+        
     }
 }
